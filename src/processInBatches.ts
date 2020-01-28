25d4fa48fd56e260ca3611614be6ef04ea43cbc1
Stefan Plösser <stefan@rarelime.de>
2020-01-28 21:13:05 +0100

    rename input param

--- src/processInBatches.ts
+++ src/processInBatches.ts
@@ -3,20 +3,20 @@
  * The values within each batch will be processed in parallel.
  * The batches are processed sequentially, one after another.
  *
- * @param {Array} values List of values to process
+ * @param {Array} inputValues List of values to process
  * @param {Function} executor An async function taking one argument
  * @param {number} batchSize How many values will be processed in parallel
  * @return {Promise<Array>} List of results, ordered like the values array
  *
  */
 export async function processInBatches<V, R>(
-  values: Array<V>,
+  inputValues: Array<V>,
   executor: (value: V) => Promise<R>,
   batchSize: number
 ): Promise<Array<R>> {
   const results: Array<R> = [];
-  for (let index = 0; index < values.length; index += batchSize) {
-    const batchItems = values.slice(index, index + batchSize);
+  for (let index = 0; index < inputValues.length; index += batchSize) {
+    const batchItems = inputValues.slice(index, index + batchSize);
     const resultsPromise = Promise.all(batchItems.map(item => executor(item)));
     // eslint-disable-next-line
     results.push(...(await resultsPromise));
